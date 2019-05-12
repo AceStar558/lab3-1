91abfb5d2faec9a409205aca3ec9e2b66411565f
AceStar558 <48096544+AceStar558@users.noreply.github.com>
2019-05-12 11:33:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,93 @@
 #include <iostream>
 #include <string>
+#include <locale>
+#include <codecvt>
 using namespace std;
 int main()
 {
-	int x,i,j,k=0;
-	cout<< "Введите кол-во верблюдов: ";
-	cin >> x;
-	k = k+x;
-	string a = "был";
-	string b = " верблюд";
-	string c;
-	i = x%10;
-	j = x%100;
-	if (((i==0)|| (i>=5 && i<=9))|| (j >= 11 && j<=14)) {
-		a = a.append("о");
-		b = b.append("ов");
-	} else {
-		if (i==1) {
-			a = a.append(c);
-			b = b.append(c);
-		} else {
-			a = a.append("а");
-			b = b.append("а");
-		}
+	locale loc("ru_RU.UTF-8");
+	wstring_convert < codecvt_utf8 < wchar_t >, wchar_t > codec;
+	string x("x");
+	int n;
+	cout << "Введите число от 1 до 100 \n";
+	cin >> n;
+	if (n%10==1 && n!=11){
+		string s = "В караване был x верблюд";
+		s.replace(s.find(x),x.length(),to_string(n));
+		wstring ws = codec.from_bytes(s);
+		s = codec.to_bytes(ws);
+		cout<< s <<endl;
 	}
-	cout<<"В караване "<<a<<" "<<k<<b<<endl;
+ else if ((n%10==2 || n%10==3 || n%10==4 ) && (n!=12 && n!=13 && n!=14)){
+		string s = "В караване был x верблюд";
+		string s1("был");
+		string s2("верблюд");
+		s.replace(s.find(s1),s1.length(),"было");
+		s.replace(s.find(s2),s2.length(),"верблюда");
+		s.replace(s.find(x),x.length(),to_string(n));
+		wstring ws = codec.from_bytes(s);
+		s = codec.to_bytes(ws);
+		cout<< s <<endl;
+	}
+	else ( n%10==5 || n%10==6 || n%10==7  || n%10==8 || n%10==9 || n%10==0 );{
+		string s = "В караване был x верблюд";
+		string s1("был");
+		string s2("верблюд");
+		s.replace(s.find(s1),s1.length(),"было");
+		s.replace(s.find(s2),s2.length(),"верблюдов");
+		s.replace(s.find(x),x.length(),to_string(n));
+		wstring ws = codec.from_bytes(s);
+		s = codec.to_bytes(ws);
+		cout<< s <<endl;
+		
+	}
 	return 0;
 }
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
